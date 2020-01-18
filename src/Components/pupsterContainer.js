68477700d515a528e6109d16d552e8b73fafd73e
Danielle Burrage <burragedanielle@gmail.com>
2020-01-18 13:57:51 -0600

    what i currently have

--- src/Components/pupsterContainer.js
+++ src/Components/pupsterContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from './NavTabs';
 
 class PupsterContainer extends Component {
     state = {
@@ -9,16 +10,7 @@ class PupsterContainer extends Component {
     handlePageChange = page => {
         this.setState({ currentPage: page });
     };
-
-    render() {
-        <Router>
-            <div>
-                <NavTabs />
-                <Route exact path="/" component={About} />
-                <Route exact path="/discover" component={Discover} />
-                <Route exact path="/search" component={Search} />
-            </div>
-        </Router>
-    }
 }
+
+export default PupsterContainer;
 
